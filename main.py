61f01ec3573835e1e804f7c8a357956b9a818cc8
Ikhwan Nashir <ikhwan.iw22@gmail.com>
2023-06-15 01:04:55 +0700

    renamed:    subscribe3.hdf5 -> freshspoiled7.h5 	new file:   freshspoiled7.hdf5 	modified:   main.py 	deleted:    subscribe_murnicampuran.hdf5

--- main.py
+++ main.py
@@ -5,38 +5,26 @@ import tensorflow as tf
 
 app = Flask(__name__)
 
-#model1 = tf.keras.models.load_model('subscribe_murnicampuran.hdf5') #murni dan tidak murni
-model2 = tf.keras.models.load_model('subscribe3.hdf5') #fresh dan tidak fresh
+model2 = tf.keras.models.load_model('freshspoiled7.hdf5') #fresh dan tidak fresh
 
 @app.route('/predict', methods=['POST'])
 def predict():
     image_file = request.files['image']
     image = Image.open(image_file)
 
-    #image1 = preprocess_image(image, (150, 150))
     image2 = preprocess_image(image, (150, 150))
 
-    #prediction1 = model1.predict(np.expand_dims(image1, axis=0))[0].tolist()
     prediction2 = model2.predict(np.expand_dims(image2, axis=0))[0].tolist()
-
 
     combined_prediction = {
         #'prediction1': prediction1,
         'prediction2': prediction2,
     }
 
-    #hasil1 = ""
     hasil2 = ""
 
-    #value1 = combined_prediction["prediction1"][0]
-    #value2 = combined_prediction["prediction1"][1]
     value3 = combined_prediction["prediction2"][0]
     value4 = combined_prediction["prediction2"][1]
-
-    #if value1 > value2:
-    #    hasil1 = "murni"
-    #else:
-    #    hasil1 = "campuran"
 
     if value3 > value4:
         hasil2 = "fresh"
@@ -50,7 +38,6 @@ def predict():
 def preprocess_image(image, size):
     image = image.resize(size)
     image = np.array(image) / 255.0
-    image = (image - 0.5) / 0.5
     return image
 
 if __name__ == '__main__':
